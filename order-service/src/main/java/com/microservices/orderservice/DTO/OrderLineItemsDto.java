213d0dd32fa19138a2a065c52895a086688931c9
zafaraftab1 <zafaraftab14581@gmail.com>
2026-04-08 11:58:30 +0530

    Add features in order service project

--- order-service/src/main/java/com/microservices/orderservice/DTO/OrderLineItemsDto.java
+++ order-service/src/main/java/com/microservices/orderservice/DTO/OrderLineItemsDto.java
@@ -1,4 +1,10 @@
 package com.microservices.orderservice.DTO;
 
+import java.math.BigDecimal;
+
 public class OrderLineItemsDto {
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
 }
